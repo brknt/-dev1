8762a8938664e57ec66e9bc4a8bb02c29555c117
brknt <brknt.gns@hotmail.com>
2023-04-24 17:23:07 +0300

    sql ödev1 eklendi

--- alistirma/script.js
+++ alistirma/script.js
@@ -1,13 +1,10 @@
-
-const kisiler = [
-    {id: 1, adi: 'Ahmet', soyadi: 'Yüksekseslekonuşma', yas: 15, cinsiyet: 'Erkek', unvan: 'Öğrenci', puan: 65},
-    {id: 2, adi: 'Sultan', soyadi: 'Hayvanlarısev', yas: 32, cinsiyet: 'Kadın', unvan: 'Öğretmen', puan: 75},
-    {id: 3, adi: 'Hatiçe', soyadi: 'Çimenlerebasma', yas: 26, cinsiyet: 'Kadın', unvan: 'Doktor', puan: 85},
-    {id: 4, adi: 'Ali', soyadi: 'Yerleretükürme', yas: 26, cinsiyet: 'Erkek', unvan: 'İşçi', puan: 75},
-    {id: 4, adi: 'Kamuran', soyadi: 'Kurallarauy', yas: 52, cinsiyet: 'Kadın', unvan: 'Hemşire', puan: 95}
-  ];
-  
-  const min = kisiler.reduce((x, y) => x < y.puan? x: y.puan, +Infinity),
-        max = kisiler.reduce((x, y) => x > y.puan? x: y.puan, -Infinity);
-  
-  console.log("min:" +min, "max:"+max)
+//// GET İsteği
+fetch("https://jsonplaceholder.typicode.com/todos")
+	.then((response) => response.json()) //parse json data
+	.then(function (todos) {
+    console.log(todos);
+    
+		todos.forEach((todo) => {
+			console.log(`title: `,todo.title); //Başlıkları console' a yazdırma
+		});
+	});
